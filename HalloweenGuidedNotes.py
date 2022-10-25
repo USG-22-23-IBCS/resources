e745f2986534ca5b180c90443f72014fa1e90435
Zack Considine <zack_considine@m015354.aw.org>
2022-10-25 09:30:40 -0700

    added guided notes for the random path algorithm

---
+++ HalloweenGuidedNotes.py
@@ -0,0 +1,64 @@
+import random
+
+class House:
+
+    def __init__(self):
+        self.rating = random.randint(1,10)
+
+    def getRating(self):
+        return self.rating
+
+def randPath(m, num):
+    #create an empty path
+    p = []
+
+    #try to add coordinates to the path
+    #if the path were to get stuck or be 'unfinished' in anyway, try again
+    #only finish once a fair path is generated
+    while (len(p) < num):
+        p = []
+
+        #keep track of the value of the path
+        #choose a random coordinate to start at
+
+
+
+        #add neighbors to the path at a randomly chosen direction
+        #keep track of whether we get stuck. If we get stuck, break
+        for i in range(num - 1):
+            
+            while True:
+                #choose a random direction and attempt to add the neighbor
+                #do not add the neighbor to the path if it is outside of the 5x5
+                #or if the neighbor is already in the path
+                #break the while loop if it was a successful addition or if stuck
+                
+    return pVal, p
+                        
+                
+  
+def main():
+    m = [[], [], [], [], []]
+    for l in m:
+        for i in range(5):
+            h = House()
+            l.append(h.getRating())
+
+    for i in range(5):
+        print(m[0][i], m[1][i], m[2][i], m[3][i], m[4][i]) 
+
+    num = int(input("How many houses?\n"))
+
+    #calculate the average rating of a house in the neighborhood
+
+    
+    #while the average of value of the house is greater than the
+    #average of the path randomly chosen, try getting another random
+    #path. stop, once it is better, and print it.
+
+            
+
+        
+
+if __name__ == "__main__":
+    main()
