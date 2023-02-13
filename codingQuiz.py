d77cd14b3d43b26719540b4179ab034e37b2f45b
Zack Considine <zack_considine@m015354.aw.org>
2023-02-13 13:21:02 -0800

    QUIZ

---
+++ codingQuiz.py
@@ -0,0 +1,35 @@
+# IBCS Python Coding Quiz
+# Name:
+
+# Read the questions below carefully. Each question describes a
+# method that you will need to define with code. In order to demonstrate
+# that your method works and test your code, you will need to 'un-comment'
+# the method call in the main() method
+
+
+# 1. Given a list of integers, the method returns True if there are
+# at least two numbers in the list that are the minimum value out of
+# everything else in the list. If there is only one minimum,
+# the method should return false.
+def twoMin():
+    return True
+
+
+# 2. Given user input as a sentence, the method calculates the number
+# of words and calculates the number of characters (excluding the spaces).
+# The method then prints out the average word length.
+def sentenceData():
+    return True
+
+
+def main():
+
+    #print(twoMin([5, 2, 4, 8, 9, 5, 7, 2, 9, 3]))
+    #print(twoMin([1, 8, 5, 8, 4, 7, 2, 9, 3]))
+    #print(twoMin([1, 1, 1, 1]))
+
+    sentenceData()
+
+
+if __name__ == "__main__":
+    main()
